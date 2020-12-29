958b5cd4738db1978d4c8bf694f313dc8d6d1593
Osama Khan <osama.khan@contour-software.com>
2020-12-29 18:01:40 +0500

    contribution by osama

--- src/com/company/pakiza/Main.java
+++ src/com/company/pakiza/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("hello world"); // added semi-colon
         System.out.println("bh jsb s"); //bug
         System.out.println("hi there");
+        System.out.println("contribution by osama");
     }
 }
